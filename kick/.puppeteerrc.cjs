332894f178072482d12e99b91bbe5bb6127115fd
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2023-09-29 21:30:39 -0600

    Update .puppeteerrc.cjs

--- kick/.puppeteerrc.cjs
+++ kick/.puppeteerrc.cjs
@@ -3,8 +3,8 @@ const { join } = require('path');
 let env = (process.env.NODE_ENV);
 
 // Changes the cache location for Puppeteer when deployed 
-if (env !== 'dev') {
+//if (env !== 'dev') {
     module.exports = {
         cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
     };
-}
+//}
